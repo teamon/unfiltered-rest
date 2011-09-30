a55925ab670e539cc51865dfe6d7e4adc042720a
Tymon Tobolski <i@teamon.eu>
2011-09-30 13:38:22 +0200

    Removed unnecessary files

--- frontend/config/environments/development.rb
+++ frontend/config/environments/development.rb
@@ -13,18 +13,9 @@ Frontend::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
 end
